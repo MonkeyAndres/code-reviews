b9595de2a16fd225d6189fddb4f6ae6f6dc21f57
Andrés Martin <monkeyandres@protonmail.com>
2018-09-20 11:56:44 +0200

    Refactor Round 1

--- matrix/src/matrix.js
+++ matrix/src/matrix.js
@@ -1,4 +1,4 @@
-import * as L from 'lodash'
+import * as _ from 'lodash'
 
 const getComplimentaryCoordinate = prop => prop === 'col' ? 'row' : 'col'
 
@@ -6,7 +6,7 @@ const getSpanKey = prop => `${prop}span`
 
 const getSpan = (cell, prop) => cell[getSpanKey(prop)]
 
-const hasSpan = (cell, prop) => !L.isNil(getSpan(cell, prop))
+const hasSpan = (cell, prop) => !_.isNil(getSpan(cell, prop))
 
 const mergedAreaIncludesCell = (mergedCell, prop, cell) =>
   mergedCell[prop] + mergedCell[getSpanKey(prop)] > cell[prop]
@@ -27,7 +27,14 @@ const isADeletion = amount => amount < 0
 
 const reallocateMergedCells = (cells, prop, index, amount) => {
   if (!isADeletion(amount)) {
-    return L.flatMap(cells, cell => {
+    return rellocateNotDeletion(cells, prop, index, amount)
+  }
+
+  return reallocateDeletion(cells, prop, index, amount)
+}
+
+const rellocateNotDeletion = (cells, prop, index, amount) => {
+    return _.flatMap(cells, cell => {
       if (cell[prop] < index && (!hasSpan(cell, prop) || cell[prop] + getSpan(cell, prop) <= index)) {
         return [cell]
       }
@@ -41,16 +48,17 @@ const reallocateMergedCells = (cells, prop, index, amount) => {
 
       return [cell]
     })
-  }
+}
 
-  return L.map(cells, cell => {
+const reallocateDeletion = (cells, prop, index, amount) => {
+  return _.map(cells, cell => {
 
     if (willBeDeleted(cell, prop, index, amount)) {
       return cell
     }
 
     if (hasSpan(cell, prop) && cell[prop] + getSpan(cell, prop) > index) {
-      const countOfDeletedOutsideMergedArea = L.max([0, (index + (-amount)) - (cell[prop] + getSpan(cell, prop))])
+      const countOfDeletedOutsideMergedArea = getCountDeletedOutsideMergedArea(cell, prop, index, amount)
       return {
         ...cell,
         [getSpanKey(prop)]: getSpan(cell, prop) - ((-amount) - countOfDeletedOutsideMergedArea)
@@ -58,22 +66,12 @@ const reallocateMergedCells = (cells, prop, index, amount) => {
     }
 
     if (isNextToDeletedArea(cell, prop, index, (-amount))) {
-      const mergedCell = L.find(cells, potentialMergedCell => {
-        const sameComplimentaryCoordinate = hasSameComplimentaryCoordinate(potentialMergedCell, cell, prop)
-        const isBeingRemoved = willBeDeleted(potentialMergedCell, prop, index, amount)
-        const hasPropSpan = hasSpan(potentialMergedCell, prop)
-        const included = mergedAreaIncludesCell(potentialMergedCell, prop, cell)
-
-        return  sameComplimentaryCoordinate
-            && isBeingRemoved
-            && hasPropSpan
-            && included
-      })
+      const mergedCell = findMergedCells(cells, cell, prop, index, amount)
 
       if (mergedCell) {
         return {
           ...cell,
-          ...L.pick(mergedCell, ['rowspan', 'colspan']),
+          ..._.pick(mergedCell, ['rowspan', 'colspan']),
           [getSpanKey(prop)]: getSpan(mergedCell, prop) - (cell[prop] - mergedCell[prop])
         }
       }
@@ -84,11 +82,27 @@ const reallocateMergedCells = (cells, prop, index, amount) => {
   })
 }
 
+const findMergedCells = (cells, cell, prop, index, amount) => {
+  return _.find(cells, potentialMergedCell => {
+    const sameComplimentaryCoordinate = hasSameComplimentaryCoordinate(potentialMergedCell, cell, prop)
+    const isBeingRemoved = willBeDeleted(potentialMergedCell, prop, index, amount)
+    const hasPropSpan = hasSpan(potentialMergedCell, prop)
+    const included = mergedAreaIncludesCell(potentialMergedCell, prop, cell)
+
+    return  sameComplimentaryCoordinate
+        && isBeingRemoved
+        && hasPropSpan
+        && included
+  })
+}
+
+const getCountDeletedOutsideMergedArea = (cell, prop, index, amount) => _.max([0, (index + (-amount)) - (cell[prop] + getSpan(cell, prop))])
+
 const removeCells = (cells, prop, index, amount) =>
-  L.filter(cells, cell => !willBeDeleted(cell, prop, index, amount))
+  _.filter(cells, cell => !willBeDeleted(cell, prop, index, amount))
 
 const moveCells = (cells, prop, index, amount) => {
-  const result = L.flatMap(cells, cell => {
+  const result = _.flatMap(cells, cell => {
     if (cell[prop] < index) {
       return [cell]
     }
@@ -101,13 +115,16 @@ const moveCells = (cells, prop, index, amount) => {
     const result = { ...cell, [prop]: cell[prop] + amount }
     return [result]
   })
+  
   return result
 }
 
 export const reallocate = (cells, prop, index, amount) => {
   let paramCells = reallocateMergedCells(cells, prop, index, amount)
+
   if (isADeletion(amount)) {
     paramCells = removeCells(paramCells, prop, index, amount)
   }
+
   return moveCells(paramCells, prop, index, amount)
 }
